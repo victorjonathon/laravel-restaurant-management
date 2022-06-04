aa1deb60edaae148719df316cc1bdd9186c37f58
Vikas <victorjonathon@gmail.com>
2022-06-04 18:24:34 +0530

    Admin template and admin login with jetstream

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,11 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
     //
     public function index(){
-        return view('Home/index');
+        return view('home/index');
+    }
+
+    public function redirects(){
+        $userType = Auth::user()->usertype;
+
+        if($userType == 0){ // User
+            return view('home/index');
+        }else{ // Admin
+            return view('admin/adminhome');
+        }
+
     }
 }
